9a1c4a9919f9ccc3d4996c9f1ab980735f88e84b
Fakhrezz <faldyriko01@gmail.com>
2021-03-16 16:42:20 +0700

    Taxi class dan objek

---
+++ Taxi/Taxi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0} ", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : yes", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine("On Duty : no", OnDuty);
+            }
+            
+            Console.WriteLine("Number of Passenger : {0} ", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Sedang mengantar penumpang", DriverName);
+        }
+
+    }
+}
---
+++ Taxi/TaxiInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class TaxiInfo
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // method
+        public void Taxi()
+        {
+
+        }
+    }
+}
